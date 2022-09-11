bdbb775003d7ac4df984d4162e244de6c9708062
YourTechBud <noorain.panjwani@gmail.com>
2022-09-11 11:53:36 +0530

    wrote the function to be deployed

---
+++ google-cloud-functions-introduction/README.md
@@ -0,0 +1,1 @@
+gcloud functions deploy greeter --trigger-http --gen2 --runtime go116 --region us-east1 --entry-point=GreeterGet
---
+++ google-cloud-functions-introduction/main.go
@@ -0,0 +1,34 @@
+package googlecloudfunctionsintroduction
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+)
+
+var greeting string = "hi"
+
+func init() {
+	functions.HTTP("GreeterGet", greetingHandler)
+}
+
+func greetingHandler(w http.ResponseWriter, r *http.Request) {
+	message := fmt.Sprintf("%s there", greeting)
+
+	// Generate some logs
+	log.Println("Received request. Sending Greeting.")
+
+	// Check if PR number is set
+	prNumber := os.Getenv("PR_NUMBER")
+	if prNumber != "" {
+		message = fmt.Sprintf("%s there from #%s", greeting, prNumber)
+	}
+
+	// Write response to client
+	w.WriteHeader(http.StatusOK)
+	_ = json.NewEncoder(w).Encode(map[string]string{"greeting": message})
+}
