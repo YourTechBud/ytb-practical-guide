9689aebbac7fa07d51845cf5025da4c2f98a2e52
Noorain Panjwani <noorain.panjwani@gmail.com>
2024-11-25 17:39:15 -0800

    removed the stale env.sh
    Signed-off-by: Noorain Panjwani <noorain.panjwani@gmail.com>

--- scripts/env.sh
+++
@@ -1,9 +0,0 @@
-export OPENAI_API_URL="https://inferix.ytbcodes.com/api/v1"
-export OPENAI_API_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ4ZWJpYS1raWRzIiwiaXNzIjoiaW5mZXJpeC55dGJjb2Rlcy5jb20iLCJpYXQiOjE1MTYyMzkwMjJ9.rgS7akZQwBg3l2CF9tJmu38lg48y6R-dT5BTUAbPVwo"
-export OPENAI_MODEL="Llama-3-8B-Instruct"
-
-
-export OPENAI_API_URL="http://localhost:4386/api/v1"
-export OPENAI_API_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ4ZWJpYS1raWRzIiwiaXNzIjoiaW5mZXJpeC55dGJjb2Rlcy5jb20iLCJpYXQiOjE1MTYyMzkwMjJ9.rgS7akZQwBg3l2CF9tJmu38lg48y6R-dT5BTUAbPVwo"
-export OPENAI_MODEL="Llama-3-8B-Instruct"
-export OPENAI_MODEL="Qwen1.5-32B-Chat"
